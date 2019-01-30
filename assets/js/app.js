7f03acf62bc9653d3fd1b67a9ca15043ff855f85
Oliver Renwick <oliver.renwick@gmail.com>
2019-01-29 23:21:34 -0500

    Sending .JSON objects to server, getting CORS error

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@ app.get("/api/tables/:table", function(req, res) {
 app.post("/api/tables", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newtable = req.body;
+  var newTable = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
